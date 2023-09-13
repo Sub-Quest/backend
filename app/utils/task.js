a84db924acd65a67e09a1ffc527c8e22d6162479
Lam Vu <lam.vu@cake.vn>
2023-09-13 23:24:18 +0700

    Define tasks

---
+++ app/utils/task.js
@@ -0,0 +1,37 @@
+const Tasks = [{
+    id: 1,
+    name: "Follow Twitter",
+    descriptions: "Ask users to follow your Twitter account",
+    group: "Twitter",
+    type: "Off-Chain",
+}, {
+    id: 2,
+    name: "Like a Tweet",
+    descriptions: "Ask users to follow your Twitter account",
+    group: "Twitter",
+    type: "Off-Chain",
+}, {
+    id: 3,
+    name: "Join Discord",
+    descriptions: "Verify users if he/she has already joined your Discord Server. *Permanent invite link is recommended",
+    group: "Discord",
+    type: "Off-Chain",
+},{
+    id: 4,
+    name: "Join Telegram",
+    descriptions: "Ask users to join your Telegram group",
+    group: "Telegram",
+    type: "Off-Chain",
+}, {
+    id: 5,
+    name: "Specific NFT Holder",
+    descriptions: "Verify if a user owns the minimum quantity of an NFT collection on a specified network",
+    group: "",
+    type: "On-Chain",
+}, {
+    id: 6,
+    name: "Minimum Token Balance",
+    descriptions: "Own at least [Amount][Token]",
+    group: "",
+    type: "On-Chain",
+}]
